7c3d28cc8b287db118dcd31f8c2e7f5327b9cb19
T0raT <tigerliu@bu.edu>
2024-08-06 23:25:05 -0400

    mp5

--- projects/mini-projects/mp5/src/components/NavBar/NavBar.test.jsx
+++ projects/mini-projects/mp5/src/components/NavBar/NavBar.test.jsx
@@ -1,26 +1,30 @@
-import { expect, test , vi } from 'vitest'
-import NavBar from './NavBar.jsx';
-import {fireEvent, render} from "@testing-library/react";
+import { expect, test, vi, afterEach} from "vitest";
+import NavBar from "./NavBar.jsx";
+import { fireEvent, render, screen , cleanup} from "@testing-library/react";
 
-test('The NavBar component should render', () => {
+// To avoid issues where vitest finds duplicate components while testing
+afterEach(async () => {
+  await cleanup();
+});
+
+test("The NavBar component should render", () => {
   const view = render(<NavBar />);
   expect(view).toMatchSnapshot();
 });
 
-test('The NavBar component should render with a title', () => {
-  const title = 'Test application';
+test("The NavBar component should render with a title", () => {
+  const title = "Test application";
   render(<NavBar title={title} />);
-
-  expect(screen.getByRole('heading')).toMatch(title);
+  expect(screen.getByRole("heading").textContent).toMatch(title);
 });
 
-test('The NavBar component should respond to button clicks', () => {
+test("The NavBar component should respond to button clicks", () => {
   const mockFunction = vi.fn();
   render(<NavBar goBack={mockFunction} openForm={mockFunction} />);
 
-  fireEvent.click(screen.getByText('< Go Back'));
+  fireEvent.click(screen.getByText("< Go Back"));
   expect(mockFunction).toHaveBeenCalled();
 
-  fireEvent.click(screen.getByText('+ Add Review'));
+  fireEvent.click(screen.getByText("+ Add Review"));
   expect(mockFunction).toHaveBeenCalledTimes(2);
 });
